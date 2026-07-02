5f8a860114dde6eb7a9fd2108f60be338f1c5b58
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2026-07-02 23:32:49 +0800

    feat: Added env variables

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GCP_PROJECT_ID = os.getenv( 'GCP_PROJECT_ID' )
+GCP_REGION = os.getenv( 'GCP_REGION', 'asia-southeast1' )
+GCS_BUCKET = os.getenv( 'GCS_BUCKET' )
+IMAGE_URI = os.getenv( 'IMAGE_URI' )
 GOOGLE_APPLICATION_CREDENTIALS = os.getenv( 'GOOGLE_APPLICATION_CREDENTIALS' )
 LOG_PREDICTIONS = True
